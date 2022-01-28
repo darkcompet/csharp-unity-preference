b362db8f8680941652ab4204ce8989964596a594
DarkCompet <darkcompet@gmail.com>
2022-01-28 12:17:29 +0700

    Make it dependent on unity-json

--- DkPrefs.cs
+++ DkPrefs.cs
@@ -1,5 +1,6 @@
 namespace Tool.Compet.Preference {
 	using Tool.Compet.Core;
+	using Tool.Compet.Json;
 	using UnityEngine;
 
 	/// This is back-compatibility preferences, store user's settings at local disk storage.
@@ -28,80 +29,80 @@ namespace Tool.Compet.Preference {
 		public static bool GetBoolean(string key, bool defaultValue = false) {
 			return GetString(key).ParseBooleanDk(defaultValue);
 		}
-	
+
 		public static int GetInt(string key, int defaultValue = 0) {
 			return GetString(key).ParseIntDk(defaultValue);
 		}
-	
+
 		public static long GetLong(string key, long defaultValue = 0L) {
 			return GetString(key).ParseLongDk(defaultValue);
 		}
-	
+
 		public static float GetFloat(string key, float defaultValue = 0F) {
 			return GetString(key).ParseFloatDk(defaultValue);
 		}
-	
+
 		public static double GetDouble(string key, double defaultValue = 0D) {
 			return GetString(key).ParseDoubleDk(defaultValue);
 		}
-	
+
 		public static string GetString(string key, string defaultValue = null) {
 			return PlayerPrefs.GetString(key, defaultValue);
 		}
-	
+
 		public static T GetJsonObj<T>(string key) where T : class {
 			return DkJsons.Json2Obj<T>(GetString(key, null));
 		}
-	
+
 		public static void PutBoolean(string key, bool value, bool alsoSave = false) {
 			PutString(key, value.ToString(), alsoSave);
 		}
-	
+
 		public static void PutInt(string key, int value, bool alsoSave = false) {
 			PutString(key, value.ToString(), alsoSave);
 		}
-	
+
 		public static void PutLong(string key, long value, bool alsoSave = false) {
 			PutString(key, value.ToString(), alsoSave);
 		}
-	
+
 		public static void PutFloat(string key, float value, bool alsoSave = false) {
 			PutString(key, value.ToString(), alsoSave);
 		}
-	
+
 		public static void PutDouble(string key, double value, bool alsoSave = false) {
 			PutString(key, value.ToString(), alsoSave);
 		}
-	
+
 		public static void PutString(string key, string value, bool alsoSave = false) {
 			PlayerPrefs.SetString(key, value);
 			if (alsoSave) {
 				PlayerPrefs.Save();
 			}
 		}
-	
+
 		public static void PutJsonObj(string key, object serializableObj, bool alsoSave = false) {
 			PutString(key, DkJsons.Obj2Json(serializableObj), alsoSave);
 		}
-	
+
 		public static bool ContainsKey(string key) {
 			return PlayerPrefs.HasKey(key);
 		}
-	
+
 		public static void DeleteKey(string key, bool alsoSave = false) {
 			PlayerPrefs.DeleteKey(key);
 			if (alsoSave) {
 				PlayerPrefs.Save();
 			}
 		}
-	
+
 		public static void Clear(bool alsoSave = true) {
 			PlayerPrefs.DeleteAll();
 			if (alsoSave) {
 				PlayerPrefs.Save();
 			}
 		}
-	
+
 		/**
 		 * Force persist preferences to local disk. Call this when we want to save preferences.
 		 * Note that, until call this function, the changes made by other functions are NOT yet persisted.
--- README.md
+++ README.md
@@ -2,8 +2,25 @@
 
 This is based on PlayerPrefs in Unity.
 
+- Features
 
-## Quick Start
+	```bash
+	- Read/Write primitive values with in-memory storage after onetime first-load from disk.
+	- Also support read/write for Json-serialized C# object.
+	```
+
+- Dependencies
+
+	```bash
+	# Tool.Compet.Json
+	https://github.com/darkcompet/csharp-unity-json
+
+	# Tool.Compet.Core
+	https://github.com/darkcompet/csharp-unity-core
+	```
+
+
+## How to install
 
 - Just include it into project by add as git-submodule.
 	
